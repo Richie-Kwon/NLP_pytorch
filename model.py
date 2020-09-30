ab90fead7e6842616a62093acfa60c9fd4ed358f
Richie-Kwon <richie.kwon1@gmail.com>
2020-09-30 20:39:40 +0100

    add MNIST

--- model.py
+++ model.py
@@ -1,58 +1,44 @@
+import torch
 import torch.nn as nn
 
 
-class Autoencoder(nn.Module):
-    
-    def __init__(self, btl_size=2):
-        self.btl_size = btl_size
-        
+class ImageClassifier(nn.Module):
+
+    def __init__(self,
+                 input_size,
+                 output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+
         super().__init__()
-        
-        self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 500),
-            nn.ReLU(),
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 500),
+            nn.LeakyReLU(),
             nn.BatchNorm1d(500),
-            nn.Linear(500, 200),
-            nn.ReLU(),
+            nn.Linear(500, 400),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(400),
+            nn.Linear(400, 300),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(300),
+            nn.Linear(300, 200),
+            nn.LeakyReLU(),
             nn.BatchNorm1d(200),
             nn.Linear(200, 100),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.BatchNorm1d(100),
             nn.Linear(100, 50),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.BatchNorm1d(50),
-            nn.Linear(50, 20),
-            nn.ReLU(),
-            nn.BatchNorm1d(20),
-            nn.Linear(20, 10),
-            nn.ReLU(),
-            nn.BatchNorm1d(10),
-            nn.Linear(10, btl_size),
+            nn.Linear(50, output_size),
+            nn.LogSoftmax(dim=-1),
         )
-        self.decoder = nn.Sequential(
-            nn.Linear(btl_size, 10),
-            nn.ReLU(),
-            nn.BatchNorm1d(10),
-            nn.Linear(10, 20),
-            nn.ReLU(),
-            nn.BatchNorm1d(20),
-            nn.Linear(20, 50),
-            nn.ReLU(),
-            nn.BatchNorm1d(50),
-            nn.Linear(50, 100),
-            nn.ReLU(),
-            nn.BatchNorm1d(100),
-            nn.Linear(100, 200),
-            nn.ReLU(),
-            nn.BatchNorm1d(200),
-            nn.Linear(200, 500),
-            nn.ReLU(),
-            nn.BatchNorm1d(500),
-            nn.Linear(500, 28 * 28),
-        )
-        
+
     def forward(self, x):
-        z = self.encoder(x)
-        y = self.decoder(z)
-        
+        # |x| = (batch_size, input_size)
+
+        y = self.layers(x)
+        # |y| = (batch_size, output_size)
+
         return y
